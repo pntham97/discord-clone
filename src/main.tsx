ca7a4b89091622ef0ed31abd2b4ee470b9211c88
pntham <pntham97@gmail.com>
2023-11-30 18:30:33 +0700

    day 1

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,5 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import "./style.css";
+import { createRoot } from "react-dom/client";
+import { Routes } from "@generouted/react-router";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+createRoot(document.getElementById("root")!).render(<Routes />);
